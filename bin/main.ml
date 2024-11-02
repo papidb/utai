8224ff6161a3323af58908a9879c98170408befc
Daniel <benjamindaniel706@gmail.com>
2024-11-02 16:21:55 +0100

    chore: add middleware

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,13 @@
+let count = ref 0
+
+let count_requests inner_handler request =
+  count := !count + 1 ;
+  inner_handler request
+
 let () =
-  Dream.run @@ Dream.logger
+  Dream.run @@ Dream.logger @@ count_requests
   @@ Dream.router
-       [ ( Dream.get "/echo/:word"
-         @@ fun request -> Dream.html @@ Dream.param request "word" )
-       ; (Dream.get "/" @@ fun _ -> Dream.html "Hi there") ]
+       [ Dream.get "/" (fun _ ->
+             Dream.html (Printf.sprintf "Saw %i request(s)!" !count) )
+       ; Dream.get "/echo/:word" (fun request ->
+             Dream.html (Dream.param request "word") ) ]
