2b8b53730bc52b5ed169720d79acf896c79ae714
Daniel <benjamindaniel706@gmail.com>
2024-11-02 16:14:44 +0100

    chore: add router

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,6 @@
 let () =
-   Dream.run (fun _ ->
-    Dream.html "Good morning, world!")
+  Dream.run @@ Dream.logger
+  @@ Dream.router
+       [ ( Dream.get "/echo/:word"
+         @@ fun request -> Dream.html @@ Dream.param request "word" )
+       ; (Dream.get "/" @@ fun _ -> Dream.html "Hi there") ]
