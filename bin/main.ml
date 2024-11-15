ab2e9b84279957e49dafb7d8d4f470d42338d747
Daniel <benjamindaniel706@gmail.com>
2024-11-15 09:59:46 +0100

    chore: extract handler into function

--- bin/main.ml
+++ bin/main.ml
@@ -4,10 +4,11 @@ let count_requests inner_handler request =
   count := !count + 1 ;
   inner_handler request
 
+let handle_home _ = Dream.html (Printf.sprintf "Saw %i request(s)!" !count)
+
+let handle_echo request = Dream.html (Dream.param request "word")
+
 let () =
   Dream.run @@ Dream.logger @@ count_requests
   @@ Dream.router
-       [ Dream.get "/" (fun _ ->
-             Dream.html (Printf.sprintf "Saw %i request(s)!" !count) )
-       ; Dream.get "/echo/:word" (fun request ->
-             Dream.html (Dream.param request "word") ) ]
+       [Dream.get "/" handle_home; Dream.get "/echo/:word" handle_echo]
